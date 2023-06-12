ca6e39689f68b326d75607cf9acea5a465796e22
micmine <micmine4@gmail.com>
2023-06-12 17:42:05 +0200

    Update tmux_interface

--- src/input/tmux.rs
+++ src/input/tmux.rs
@@ -1,22 +1,22 @@
-use tmux_interface::TmuxCommand;
+use tmux_interface::{CapturePane, Tmux};
 
 /// Return the output of a pane
 #[must_use]
 pub fn get_tmux_pane_content(target: &str) -> Option<String> {
-    let tmux = TmuxCommand::new();
-
-    tmux.capture_pane()
-        .stdout()
-        //.escape_sequences() // shell colors
-        .start_line("-")
-        .join()
-        .target_pane(target)
-        .output()
-        .map_or_else(
-            |o| Some(o.to_string()),
-            |_| {
-                println!("Unable to read from tmux target: {target}");
-                None
-            },
-        )
+    Tmux::with_command(
+        CapturePane::new()
+            .stdout()
+            //.escape_sequences() // shell colors
+            .start_line("-")
+            .join()
+            .target_pane(target),
+    )
+    .output()
+    .map_or_else(
+        |o| Some(o.to_string()),
+        |_| {
+            println!("Unable to read from tmux target: {target}");
+            None
+        },
+    )
 }
