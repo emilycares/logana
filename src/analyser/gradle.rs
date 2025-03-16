9a76dabf90a5530d417fe5767f276a7be7ed73fb
emilycares <emilydoescare@gmail.com>
2025-03-16 20:13:17 +0100

    Update gradle multi module and kotlin

--- src/analyser/gradle.rs
+++ src/analyser/gradle.rs
@@ -8,14 +8,22 @@ pub fn analyse(log: &str, project_dir: &str) -> Vec<types::Message> {
     let lines = log.lines().collect::<Vec<&str>>();
     let lines = lines.as_slice();
     let line_len = &lines.len();
+    let mut current_module = None;
 
     for i in 0..*line_len {
         if let Some(line) = lines.get(i) {
             let line = line.trim();
 
+            if line.starts_with("> Task :") {
+                let line = line.trim_start_matches("> Task :");
+                if let Some((module, _)) = line.split_once(":") {
+                    current_module = Some(module);
+                }
+            }
+
             // Only works with "./gradlew test --info"
             if line.ends_with("FAILED") {
-                parse_failed_test(i, line_len, lines, project_dir, &mut errors);
+                parse_failed_test(i, line_len, lines, project_dir, current_module, &mut errors);
             }
 
             if line.starts_with(project_dir) {
@@ -29,7 +37,14 @@ pub fn analyse(log: &str, project_dir: &str) -> Vec<types::Message> {
     errors
 }
 
-fn parse_failed_test(i: usize, line_len: &usize, lines: &[&str], project_dir: &str, errors: &mut Vec<types::Message>) {
+fn parse_failed_test(
+    i: usize,
+    line_len: &usize,
+    lines: &[&str],
+    project_dir: &str,
+    current_module: Option<&str>,
+    errors: &mut Vec<types::Message>,
+) {
     let mut msg = None;
     'test_case: for i in i..*line_len {
         if let Some(line) = lines.get(i) {
@@ -49,24 +64,41 @@ fn parse_failed_test(i: usize, line_len: &usize, lines: &[&str], project_dir: &s
             }
             if let Some((_, line)) = line.split_once("app//") {
                 if let Some((path, rest)) = line.split_once("(") {
-                    if let Some((filename, line)) = rest.split_once(".java:") {
+                    let mut language = "java";
+                    if let Some((_, new_lang)) = rest.split_once(".") {
+                        if let Some((new_lang, _)) = new_lang.rsplit_once(":") {
+                            language = new_lang;
+                        }
+                    }
+
+                    if let Some((filename, line)) = rest.split_once(&format!(".{language}:")) {
                         let line_number = line.trim_end_matches(")");
 
+                        let language_identifier = match language {
+                            "java" => "java",
+                            "kt" => "kotlin",
+                            a => a,
+                        };
                         if let Some((class_path, _)) = path.split_once(filename) {
+                            let module = match current_module {
+                                None => "",
+                                Some(module) => &format!("/{module}"),
+                            };
                             let path = format!(
-                                "{}/src/test/java/{}{}.java",
+                                "{}{}/src/test/{}/{}{}.{}",
                                 project_dir,
+                                module,
+                                language_identifier,
                                 class_path.replace(".", "/"),
-                                filename
+                                filename,
+                                language
                             );
                             if let Some(error) = msg {
                                 errors.push(types::Message {
                                     error: error.trim().to_owned(),
                                     locations: vec![types::Location {
                                         path: path.to_string(),
-                                        row: line_number
-                                            .parse::<usize>()
-                                            .unwrap_or_default(),
+                                        row: line_number.parse::<usize>().unwrap_or_default(),
                                         col: 0,
                                     }],
                                 })
@@ -156,6 +188,26 @@ mod tests {
                     path: "/home/emily/tmp/gradle-test/src/test/java/org/example/AppTest.java"
                         .to_string(),
                     row: 13,
+                    col: 0
+                }]
+            }]
+        );
+    }
+
+    #[test]
+    fn should_find_test_error_in_kotlin() {
+        static LOG: &str = include_str!("../../tests/gradle_kotlin.log");
+        let result = analyse(LOG, "/home/emily/tmp/gradle-test");
+
+        assert_eq!(
+            result,
+            vec![types::Message {
+                error: "<true> but was: <false>".to_string(),
+                locations: vec![types::Location {
+                    path:
+                        "/home/emily/tmp/gradle-test/common/src/test/kotlin/org/example/AppTest.kt"
+                            .to_string(),
+                    row: 14,
                     col: 0
                 }]
             }]
